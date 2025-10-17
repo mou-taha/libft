32408352af60e6338ba36368c86b4fb6c4155097
taha1337 <t.mousnia@gmail.com>
2025-10-17 07:47:10 +0100

    adds

---
+++ ft_memchr.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void *ft_memchar(const void *s, int c,unsigned int n)
+{
+    unsigned char *s = (unsigned char*)s;
+    
+}
---
+++ ft_strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ft_strcmp(const char *s1, const char *s2)
+{
+    int i = 0;
+
+    while (s1[i] || s2[i])
+    {
+        if (s1[i] != s2[i])
+            return (unsigned char)s1[i] - (unsigned char)s2[i];
+        i++;
+    }
+    return 0;
+}
+
+int main(void)
+{
+    char s1[6]="a";
+    char s2[6]="A";
+    printf("res : %d\n",ft_strcmp(s1,s2));
+    return 0;
+}
